b1e9b5c63d5e956787b68de9d70e86343053ae85
joyfulsummer <41123248+joyfulsummer@users.noreply.github.com>
2018-10-12 13:31:18 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+# This file keeps list of dependencies required for the Elasticsearch python client, Youtube API client, and Flask
+# install package for Youtube API scraping 
+pip install pafy
+pip install --upgrade google-api-python-client
+pip install --upgrade oauth2client
+sudo pip install --upgrade youtube_dl
+
+# install aws client
+sudo apt install awscli
+
+# install elasticsearch
+pip install "elasticsearch-dsl>=6.0.0"
+pip install boto3
+
+# install flask 
+pip install flask
